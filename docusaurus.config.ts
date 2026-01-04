687c573d2ed033b9cfd05df321c77e231dea3a63
gaopfEditer <1241961245@qq.com>
2026-01-04 18:25:14 +0800

    upt

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -52,10 +52,13 @@ const config: Config = {
       },
       hideOnScroll: true,
       items: [
-        { label: '笔记', position: 'right', to: 'docs/skill' },
-        // { label: '博客', position: 'right', to: 'blog' },
+        { label: '计划', position: 'right', to: 'plan' },
+        { label: '信息', position: 'right', to: 'info' },
+        { label: '投资', position: 'right', to: 'invest' },
+        { label: '运营', position: 'right', to: 'operate' },
+        { label: '杂谈', position: 'right', to: 'blog' },
         { label: '项目', position: 'right', to: 'project' },
-        { label: '友链', position: 'right', to: 'friends' },
+        { label: '技术', position: 'right', to: 'docs/skill' },
         // { label: '关于', position: 'right', to: 'about' },
         // {
         //   label: '更多',
